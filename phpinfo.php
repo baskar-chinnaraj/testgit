184ff943f53be898483c214e0c8b300e7487bcf3
Baskar Chinnaraj <baskar_c@HCL.COM>
2018-11-09 11:28:02 +0530

    hotfix3 changes removed completely

--- phpinfo.php
+++ phpinfo.php
@@ -4,6 +4,5 @@ echo '123 added from develop';
 echo "hot fix1";
 echo "add echo from master file";
 echo "523443";
-echo "hot fix2 second time";
-echo "hot fix3 ";
+echo "hot fix2 second time- hot fix 3 removed from master";
 ?>
